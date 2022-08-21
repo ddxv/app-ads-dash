9cf6147b4f5ab63ab94b18d416d8318a0d970dc3
james <james@bubbleye.com>
2022-08-21 20:53:24 +0800

    allow not sending a xval as timestamp

--- plotter/plotter.py
+++ plotter/plotter.py
@@ -46,99 +46,99 @@ def overview_plot(
     else:
         show_bar_legend = False
     # logger.warning(f"PLOT TYPE: {bar_column=}, {safe_y_vals=}")
-    for y_val in safe_y_vals:
-        if "date" in df.columns:
-            xaxis_col = "date"
-        # symbol_int expected order: 0, 101, 302, 3, 104
-        if y_val not in bar_columns:
-            symbol_int += 1
-            if symbol_int > 100:
-                symbol_int += 200
-            if symbol_int < 100 and symbol_int > 0:
-                symbol_int += 100
-            if symbol_int > 400:
-                symbol_int -= 500
-            # logger.info(f":{symbol_int=}")
-            # symbol_int +=1
-        main_color_cat = ordered.head(20).index.tolist()
-        for i in range(len(main_color_cat)):
-            color = COLORS[i]
-            cat = main_color_cat[i]
-            df.loc[df.id == cat, "color"] = color
-        df.loc[df.color.isnull(), "color"] = COLORS[len(main_color_cat)]
-        cdf = df[["id", "color"]].drop_duplicates()
-        pdf = pd.pivot_table(
-            df, index=[xaxis_col], columns="id", values=y_val
-        ).reset_index()
-        pdf = pdf.sort_values(xaxis_col)
-        missing_plot_columns = [x for x in plot_dims if x not in pdf.columns]
-        for mcol in missing_plot_columns:
-            pdf[mcol] = np.nan
-        for my_id in ordered.index:
-            # BAR / Y-AXIS 1
-            if y_val in bar_columns:
-                yaxis1_col.append(y_val)
-                if is_dollar(y_val):
-                    y1_tickformat = "$f"
-                mytype = "bar"
-                temp = df[df.id == my_id]
-                my_dict = dict(
-                    type=mytype,
-                    x=temp[xaxis_col],
-                    y=temp[y_val],
-                    name=my_id,
-                    opacity=0.8,
-                    showlegend=show_bar_legend,
-                    legendgroup=my_id,
-                    marker=dict(
-                        color=temp["color"],
-                    ),
-                )
-            # SCATTER / Y-AXIS 2
-            else:
-                if stack_bars:
-                    name_id = my_id.split(" ")[-1:][0]
-                else:
-                    name_id = my_id
-                if color_dims:
-                    if len([x for x in safe_y_vals if x not in bar_columns]) > 1:
-                        name = name_id + " " + y_val
-                    else:
-                        name = name_id
-                    marker_dict = dict(
-                        color=cdf[cdf.id == my_id].color.values[0], symbol=symbol_int
+    if xaxis_col in df.columns:
+        for y_val in safe_y_vals:
+            # symbol_int expected order: 0, 101, 302, 3, 104
+            if y_val not in bar_columns:
+                symbol_int += 1
+                if symbol_int > 100:
+                    symbol_int += 200
+                if symbol_int < 100 and symbol_int > 0:
+                    symbol_int += 100
+                if symbol_int > 400:
+                    symbol_int -= 500
+                # logger.info(f":{symbol_int=}")
+                # symbol_int +=1
+            main_color_cat = ordered.head(20).index.tolist()
+            for i in range(len(main_color_cat)):
+                color = COLORS[i]
+                cat = main_color_cat[i]
+                df.loc[df.id == cat, "color"] = color
+            df.loc[df.color.isnull(), "color"] = COLORS[len(main_color_cat)]
+            cdf = df[["id", "color"]].drop_duplicates()
+            pdf = pd.pivot_table(
+                df, index=[xaxis_col], columns="id", values=y_val
+            ).reset_index()
+            pdf = pdf.sort_values(xaxis_col)
+            missing_plot_columns = [x for x in plot_dims if x not in pdf.columns]
+            for mcol in missing_plot_columns:
+                pdf[mcol] = np.nan
+            for my_id in ordered.index:
+                # BAR / Y-AXIS 1
+                if y_val in bar_columns:
+                    yaxis1_col.append(y_val)
+                    if is_dollar(y_val):
+                        y1_tickformat = "$f"
+                    mytype = "bar"
+                    temp = df[df.id == my_id]
+                    my_dict = dict(
+                        type=mytype,
+                        x=temp[xaxis_col],
+                        y=temp[y_val],
+                        name=my_id,
+                        opacity=0.8,
+                        showlegend=show_bar_legend,
+                        legendgroup=my_id,
+                        marker=dict(
+                            color=temp["color"],
+                        ),
                     )
+                # SCATTER / Y-AXIS 2
                 else:
-                    if len(plot_dims) == 1:
-                        name = y_val
+                    if stack_bars:
+                        name_id = my_id.split(" ")[-1:][0]
                     else:
-                        name = y_val + " " + name_id
-                    marker_dict = dict(color=COLORS[y_color_int], symbol=symbol_int)
-                # name = name.replace(dims_common_str, "")
-                if is_percent(y_val):
-                    y2_tickformat = "%.2f"
-                if is_dollar(y_val):
-                    y2_tickformat = "$.3f"
-                if y_val == "count":
-                    line_dict = {"shape": "hv"}
-                else:
-                    line_dict = {"shape": "linear", "width": 1}
-                yaxis2_col.append(y_val)
-                my_dict = dict(
-                    type="scatter",
-                    x=pdf[xaxis_col],
-                    y=pdf[my_id],
-                    opacity=1,
-                    marker=marker_dict,
-                    line=line_dict,
-                    legendgroup=name_id,
-                    showlegend=True,
-                    name=name,
-                    yaxis="y2",
-                    mode="markers+lines",
-                )
-            fig.add_trace(my_dict)
-        y_color_int += 1
+                        name_id = my_id
+                    if color_dims:
+                        if len([x for x in safe_y_vals if x not in bar_columns]) > 1:
+                            name = name_id + " " + y_val
+                        else:
+                            name = name_id
+                        marker_dict = dict(
+                            color=cdf[cdf.id == my_id].color.values[0],
+                            symbol=symbol_int,
+                        )
+                    else:
+                        if len(plot_dims) == 1:
+                            name = y_val
+                        else:
+                            name = y_val + " " + name_id
+                        marker_dict = dict(color=COLORS[y_color_int], symbol=symbol_int)
+                    # name = name.replace(dims_common_str, "")
+                    if is_percent(y_val):
+                        y2_tickformat = "%.2f"
+                    if is_dollar(y_val):
+                        y2_tickformat = "$.3f"
+                    if y_val == "count":
+                        line_dict = {"shape": "hv"}
+                    else:
+                        line_dict = {"shape": "linear", "width": 1}
+                    yaxis2_col.append(y_val)
+                    my_dict = dict(
+                        type="scatter",
+                        x=pdf[xaxis_col],
+                        y=pdf[my_id],
+                        opacity=1,
+                        marker=marker_dict,
+                        line=line_dict,
+                        legendgroup=name_id,
+                        showlegend=True,
+                        name=name,
+                        yaxis="y2",
+                        mode="markers+lines",
+                    )
+                fig.add_trace(my_dict)
+            y_color_int += 1
     if stack_bars:
         bar_type = "group"
     else:
