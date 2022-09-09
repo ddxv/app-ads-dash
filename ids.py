6d15eacb29190841cdc7ebb2b8cf7852c7b4553b
james <james@bubbleye.com>
2022-09-10 00:00:14 +0800

    add ids

---
+++ ids.py
@@ -0,0 +1,16 @@
+# Singles
+DATE_PICKER_RANGE = "date-picker-range"
+
+# Tab IDs
+LATEST_UPDATES = "latest-updates"
+DEVELOPERS = "developers"
+UPDATED_AT = "updated-at"
+UPDATED_HISTOGRAM = "updated-histogram"
+DEVELOPERS_SEARCH = "developers-search"
+
+# Tab Options
+AFFIX_PLOT = "-plot"
+AFFIX_GROUPBY = "-groupby"
+AFFIX_GROUPBY_TIME = "-groupby-time"
+AFFIX_SWITCHES = "-switches"
+AFFIX_TABLE = "-table"
