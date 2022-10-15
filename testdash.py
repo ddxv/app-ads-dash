198d6787ed3d9f4c23b2411497e5f7471b24fc6a
james <james@bubbleye.com>
2022-10-15 17:05:47 +0800

    Retry old structure

--- testdash.py
+++ testdash.py
@@ -2,8 +2,10 @@ import dash
 import dash_bootstrap_components as dbc
 from dash import dcc
 from config import get_logger
-from server import server
-from dash_bootstrap_templates import load_figure_template
+
+# from server import server
+# from dash_bootstrap_templates import load_figure_template
+from app import app
 
 
 logger = get_logger(__name__)
@@ -11,17 +13,17 @@ logger = get_logger(__name__)
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
 
 
-# This loads the "cyborg" themed figure template from dash-bootstrap-templates library,
-# adds it to plotly.io and makes it the default figure template.
-load_figure_template("darkly")
+# # This loads the "cyborg" themed figure template from dash-bootstrap-templates
+# # adds it to plotly.io and makes it the default figure template.
+# load_figure_template("darkly")
 
-app = dash.Dash(
-    name=__name__,
-    server=server,
-    external_stylesheets=[dbc.themes.DARKLY, dbc_css],
-    use_pages=True,
-    suppress_callback_exceptions=True,
-)
+# app = dash.Dash(
+#     name=__name__,
+#     server=server,
+#     external_stylesheets=[dbc.themes.DARKLY, dbc_css],
+#     use_pages=True,
+#     suppress_callback_exceptions=True,
+# )
 
 
 app.layout = dbc.Container(
