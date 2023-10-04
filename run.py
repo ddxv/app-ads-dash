40ec63a36b7c6308df6e2f32f0f71294e2d2a0b8
james <james@bubbleye.com>
2023-10-04 13:54:06 +0800

    Add app history as html table

--- run.py
+++ run.py
@@ -76,8 +76,8 @@ def apps_home():
     return render_template("apps_home.html", cats=category_dicts)
 
 
-@server.route("/<store>/<app_id>")
-def app_detail(store, app_id):
+@server.route("/apps/<app_id>")
+def app_detail(app_id):
     # Fetch app details from the database using store and app_id
     app_id = "com.puzzlepro.at"
     app = get_single_app(app_id)
